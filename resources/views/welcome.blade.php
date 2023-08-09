8261afd823a0e33a1294711e8d5e01a40d2a1195
amitabhsolwin <amitabhsolwin@gmail.com>
2023-08-09 16:40:34 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,25 @@
                  <li data-transition="fade">
                     <img src="/assets/images/slider/slide-1.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
                 </li>
+                 <li data-transition="fade">
+                    <img src="/assets/images/slider/slide-2.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
+                </li>
+                 <li data-transition="fade">
+                    <img src="/assets/images/slider/slide-3.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
+                </li>
+                 <li data-transition="fade">
+                    <img src="/assets/images/slider/slide-4.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
+                </li>
+                 <li data-transition="fade">
+                    <img src="/assets/images/slider/slide-5.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
+                </li>
+                 <li data-transition="fade">
+                    <img src="/assets/images/slider/slide-6.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
+                </li>
+                 <li data-transition="fade">
+                    <img src="/assets/images/slider/slide-7.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
+                </li>
+                
                 <li data-transition="fade">
                     <img src="/assets/images/slider/img-00.jpg" data-bgposition="left center" data-duration="8000" data-bgpositionend="right center" alt="">
                 </li>
