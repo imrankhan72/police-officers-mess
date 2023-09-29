b794fbd961fe56638d6b4422903942499dc1e225
imrankhan72 <imrankhan.72@gmail.com>
2023-09-29 11:47:48 +0530

    Update site.blade.php
    Added queryable bot

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -236,5 +236,9 @@
 
 <!-- Custom jQuery -->
 <script type="text/javascript" src="/assets/js/scripts.js"></script>
+<!-- Start of queryable.xyz script -->
+<script> window.embedId="9a3f5559-e313-4e45-8e92-650918e95c5c"; </script>
+<script src="https://query-able.xyz/embed.js"></script>
+<!-- End of queryable.xyz script -->    
 </body>
 </html>
