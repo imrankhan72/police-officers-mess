95f8abec131ce9f881f7d9b460f0475f99a2b194
imrankhan72 <imrankhan.72@gmail.com>
2023-10-20 12:13:58 +0530

    added sms api

--- routes/web.php
+++ routes/web.php
@@ -25,41 +25,7 @@ Route::view('/gallery','gallery');
 Route::view('/contact-us','contact-us');
 Route::view('/book-now','book');
 
-
-Route::get('gcd',function (Request $request){
-    $mobile_no = $request->get('mobile_no');
-    $client_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/User_Registration?Mobile_No=$mobile_no")->collect()->first();
-    $client_id = $client_details['Client_ID'];
-    $hotel_bill_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientHotelBills?Client_ID=$client_id")->collect();
-    $restaurant_bill_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientFoodBills?Client_ID=$client_id")->collect();
-    $total_outstanding = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientOutStanding?Client_ID=$client_id")->collect()->first();
-    if (!$total_outstanding){
-        $total_outstanding = $hotel_bill_details->sum('NetAmt') + $restaurant_bill_details->sum('NetAmt');
-    }else{
-        $total_outstanding =  $total_outstanding['Rwmanig_Amount'];
-    }
-
-    return response()->json([
-        'client_id' => $client_id,
-        'hotel_bill_details' => $hotel_bill_details,
-        'restaurant_bill_details'=> $restaurant_bill_details,
-        'total_outstanding' =>$total_outstanding
-
-    ]);
-
-});
-
-Route::get('call_gcd',function (Request $request){
-
- return Http::get('https://pom.bharatunited.com/gcd', [
-        'mobile_no' => 8220291274
-    ]);
-});
-
-
-
 Route::get('send_sms',function (){
-
     $message = "Dear Imaad Your login code is 5560 to pay POM bill. Please don't share it with anyone. Regards POMBP";
     return Http::timeout(500)->post("https://msdgweb.mgov.gov.in/esms/sendsmsrequestDLT", [
         'username' => 'DITMP-OCCTNSS',
@@ -100,18 +66,24 @@ Route::post('/bill-details', function (Request $request) {
 //         return back()->with('message', 'Wrong OTP');
 //     }
 
-//    $client_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/User_Registration?Mobile_No=8106986039")->collect()->first();
 
     $client_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/User_Registration?Mobile_No=$mobile_no")->collect()->first();
-    $client_id = $client_details['Client_ID'];
-    $hotel_bill_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientHotelBills?Client_ID=$client_id")->collect();
-    $restaurant_bill_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientFoodBills?Client_ID=$client_id")->collect();
-    $total_outstanding = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientOutStanding?Client_ID=$client_id")->collect()->first();
-    if (!$total_outstanding){
-        $total_outstanding = $hotel_bill_details->sum('NetAmt') + $restaurant_bill_details->sum('NetAmt');
-    }else{
-        $total_outstanding =  $total_outstanding['Rwmanig_Amount'];
+    if($client_details){
+        $client_id = $client_details['Client_ID'];
+        $hotel_bill_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientHotelBills?Client_ID=$client_id")->collect();
+        $restaurant_bill_details = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientFoodBills?Client_ID=$client_id")->collect();
+        $total_outstanding = Http::get("http://pom.dvinfosoft.com/User_API.asmx/ClientOutStanding?Client_ID=$client_id")->collect()->first();
+        if (!$total_outstanding){
+            $total_outstanding = $hotel_bill_details->sum('NetAmt') + $restaurant_bill_details->sum('NetAmt');
+        }else{
+            $total_outstanding =  $total_outstanding['Rwmanig_Amount'];
+        }
+    }else {
+        $restaurant_bill_details = null;
+        $hotel_bill_details = null;
+        $total_outstanding = null;
     }
+
     return view('bill_details', compact('client_details','hotel_bill_details', 'restaurant_bill_details','total_outstanding', 'mobile_no'));
 });
 
