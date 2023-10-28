085a8d4f59dac8a3b8f7c04728a2759c4ea28b48
imrankhan72 <imrankhan.72@gmail.com>
2023-10-28 22:35:26 +0530

    added sms api

--- public/sms.php
+++ public/sms.php
@@ -1,15 +1,15 @@
 <?php
-    header('Content-Type: text/html;');
-    $username='DITMP-OCCTNS'; //username of the department
-    $password='Cctns@12345'; //password of the department
-    $senderid='OCCTNS'; //senderid of the deparment
-    $message="Dear Bhaskar Your login code is 5560 to pay POM bill. Please don't share it with anyone. Regards POMBPL"; //message content
-    $messageUnicode="à¤®à¥‹à¤¬à¤¾à¤‡à¤²à¤¸à¥‡à¤µà¤¾à¤®à¥‡à¤‚à¤†à¤ªà¤•à¤¾à¤¸à¥à¤µà¤¾à¤—à¤¤à¤¹à¥ˆ "; //message content in unicode
-    $mobileno="919826445006"; //if single sms need to be send use mobileno keyword
-    $mobileNos= "86XXXXXX72,79XXXXXX00"; //if bulk sms need to send use mobileNos as keyword and mobile number seperated by commas as value
-    $deptSecureKey= '3d8183ac-8495-4e80-ac8a-2362e0da9838'; //departsecure key for encryption of message...
-    $encryp_password=sha1(trim($password));
-    $templateid='1307169693372298480'; //your DLT registered templateid for this perticular message
+//    header('Content-Type: text/html;');
+//    $username='DITMP-OCCTNS'; //username of the department
+//    $password='Cctns@12345'; //password of the department
+//    $senderid='OCCTNS'; //senderid of the deparment
+//    $message="Dear Bhaskar Your login code is 5560 to pay POM bill. Please don't share it with anyone. Regards POMBPL"; //message content
+//    $messageUnicode="à¤®à¥‹à¤¬à¤¾à¤‡à¤²à¤¸à¥‡à¤µà¤¾à¤®à¥‡à¤‚à¤†à¤ªà¤•à¤¾à¤¸à¥à¤µà¤¾à¤—à¤¤à¤¹à¥ˆ "; //message content in unicode
+//    $mobileno="919826445006"; //if single sms need to be send use mobileno keyword
+//    $mobileNos= "86XXXXXX72,79XXXXXX00"; //if bulk sms need to send use mobileNos as keyword and mobile number seperated by commas as value
+//    $deptSecureKey= '3d8183ac-8495-4e80-ac8a-2362e0da9838'; //departsecure key for encryption of message...
+//    $encryp_password=sha1(trim($password));
+//    $templateid='1307169693372298480'; //your DLT registered templateid for this perticular message
 
 
 
@@ -52,6 +52,7 @@
     post_to_url("https://msdgweb.mgov.gov.in/esms/sendsmsrequestDLT",$data); //calling post_to_url to send sms
     }
 
+    $message="Dear Tittu Your login code is 5560 to pay POM bill. Please don't share it with anyone. Regards POMBPL";
     sendSingleSMS('DITMP-OCCTNS',sha1(trim('Cctns@12345')),'OCCTNS',$message,'9826445006','3d8183ac-8495-4e80-ac8a-2362e0da9838','1307169693372298480');
 
 
