d6006697dde3b74107dc915b62b8ec93f5571b96
ayushchaturvedi1998 <130469230+ayushchaturvedi1998@users.noreply.github.com>
2023-12-26 15:59:22 +0530

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -34,10 +34,10 @@
                         <div class="text" style="margin-top: 0px">
                             <h2 class="heading">ABOUT The Police Officers Mess</h2>
                             <div class="desc">
-                                <p>The Police Officers Mess located in Bhopal is a prestigious institution designed to provide a comfortable and relaxing environment for Police Officers and their guests. The Mess is equipped with a wide range of facilities that cater to the various needs of its guests. .  </p>
-                                <p>The Mess features a spacious and well-equipped party hall and beautifully landscaped garden with a lake view, perfect for hosting social events and gatherings. The dining hall is elegantly furnished, providing a pleasant atmosphere for enjoying meals with colleagues or guests. The Mess also offers cozy and comfortable stay rooms for its members, including single and suite options.</p>
-                                <p>For those looking to unwind and socialize, the Mess features a mini party area and a lounge. The Doom area provides a tranquil setting for meditation and reflection. Additionally, there is a gym available for those who wish to maintain their fitness regimen.
-                                    A police officers mess in Bhopal is a versatile and welcoming establishment, which provides its attendees with a comfortable and enjoyable experience, providing a comfortable and welcoming environment.</p>
+                                <p>The Police Officers Mess, Bhopal is a prestigious institution designed to provide a comfortable and relaxing stay for Police Officers and their guests. The Mess is equipped with a wide range of facilities that caters to the various needs of its guests. .  </p>
+                                <p>The Mess features a spacious and well-equipped party hall and beautifully landscaped garden with a lake view, perfect for hosting social events and gatherings. The dining hall is elegantly furnished, providing a pleasant atmosphere for enjoying meals with colleagues or guests. The Mess also offers cozy and comfortable rooms for its members, including single rooms and suite options.</p>
+                                <p>For those looking to unwind and socialize, the Mess features a mini party area and a lounge. The Dome area provides a tranquil setting for meditation and reflection. Additionally, there is a gym available for those who wish to maintain their fitness regimen.
+                                    Police officers mess in Bhopal is a versatile and welcoming establishment, which provides its attendees with a comfortable and enjoyable experience, providing a comfortable and welcoming environment.</p>
                             </div>
                         </div>
 
