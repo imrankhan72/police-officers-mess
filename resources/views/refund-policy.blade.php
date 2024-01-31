a7367b97d9067e6e543365109e02f3353f29a189
imrankhan72 <imrankhan.72@gmail.com>
2024-01-31 16:31:00 +0530

    Update refund-policy.blade.php

--- resources/views/refund-policy.blade.php
+++ resources/views/refund-policy.blade.php
@@ -19,11 +19,12 @@
         <section class="section-contact">
             <div class="container">
                 <div class="contact">
-                    <div class="text-center">
+                    <div>
                        <ul>
-  <li>This Website and the information, tools, and material contained in it (this "Site") are not directed to, or intended for distribution to or use by, any person or entity who is a citizen or resident of or located in any jurisdiction where such distribution, publication, availability, or use would be contrary to law or regulation or which would subject Police Officer Mess and its affiliates collectively "Police Officer Mess" to any registration or licensing requirement within such jurisdiction.</li>
-  <li>This Site is subject to periodic update and revision. Materials should only be considered current as of the date of initial publication appearing thereon, without regard to the date on which you may access the information.</li>
-  <li>Police Officer Mess maintains the right to delete or modify information on this Site without prior notice.</li>
+  <li>50% of the booking amount shall be refundable if Cancellation is done within a month for Banquets and Lawns.</li>
+  <li>	No refunds shall be made, if cancellation is done after a month. </li>
+  <li>For cancellations of refundable bookings, the refund processing will take 5-7 working days.</li>
+  <li>•	If the reservation is not confirmed, we will not charge anything.</li>                         
 </ul>
 
                     </div>
