8e504049181adb0f936184dd3a05203632659ba1
imrankhan72 <imrankhan.72@gmail.com>
2024-01-31 15:19:09 +0530

    Create disclaimer.blade.php

---
+++ resources/views/disclaimer.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.site')
+@section('content')
+    <main>
+        <!-- SUB BANNER -->
+        <section class="section-sub-banner bg-9">
+            <div class="sub-banner">
+                <div class="container">
+                    <div class="text text-center">
+                        <h2>Cancellations</h2>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+        <!-- END / SUB BANNER -->
+
+        <!-- CONTACT -->
+        <section class="section-contact">
+            <div class="container">
+                <div class="contact">
+                    <div class="text-center">
+                       <p>
+                           In the event of changes to your plans, please ensure to inform the Police Officers’ Mess promptly regarding any cancellations or amendments to your booking.
+                       </p>
+                        <p>Cancellations made 30 days or more before the scheduled booking date will be considered for Banquets and Lawns.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END / CONTACT -->
+    </main>
+@stop
