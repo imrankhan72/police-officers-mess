df522e1bc016d1977e90ec792346cbd5d96fee81
imrankhan72 <imrankhan.72@gmail.com>
2024-01-31 15:32:20 +0530

    Update disclaimer.blade.php

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -6,7 +6,7 @@
             <div class="sub-banner">
                 <div class="container">
                     <div class="text text-center">
-                        <h2>Cancellations</h2>
+                        <h2>Disclaimer</h2>
                     </div>
                 </div>
 
@@ -20,10 +20,7 @@
             <div class="container">
                 <div class="contact">
                     <div class="text-center">
-                       <p>
-                           In the event of changes to your plans, please ensure to inform the Police Officers’ Mess promptly regarding any cancellations or amendments to your booking.
-                       </p>
-                        <p>Cancellations made 30 days or more before the scheduled booking date will be considered for Banquets and Lawns.</p>
+                        <p>This Website and the information, tools and material contained in it (this "Site") are not directed to, or intended for distribution to or use by, any person or entity who is a citizen or resident of or located in any jurisdiction where such distribution, publication, availability or use would be contrary to law or regulation or which would subject Police Officer Mess its affiliates collectively " Police Officer Mess " to any registration or licensing requirement within such jurisdiction. This Site is subject to periodic update and revision. Materials should only be considered current as of the date of initial publication appearing thereon, without regard to the date on which you may access the information. Police Officer Mess maintains the right to delete or modify information on this Site without prior notice.</p>
                     </div>
                 </div>
             </div>
