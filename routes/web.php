d762a06a74ed9b9ecd4dc96b062baf98f11ebd0c
imrankhan72 <imrankhan.72@gmail.com>
2024-02-22 13:45:20 +0530

    added payment gateway

--- routes/web.php
+++ routes/web.php
@@ -197,8 +197,8 @@ Route::get('/booking-status-otp',function (){
 });
 Route::get('/make-payment', function (Request $request) {
     $params = [
-        'chargeAmount' => 70,
-        'chargeHead1'=> 70,
+        'chargeAmount' => $request->get('amount'),
+        'chargeHead1'=> $request->get('amount'),
         'currencyCode' => '356',
         'desc' => $request->get('name'),
         'dueDate' => Carbon::now()->format('d/m/Y'),
