8c6b44b25a4a18a525059b594b35fe678b72a4bc
imrankhan72 <imrankhan.72@gmail.com>
2024-02-28 13:34:34 +0530

    Create payment-success.blade.php

---
+++ resources/views/payment-success.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.site')
+@section('content')
+    <main>
+        <!-- SUB BANNER -->
+        <section class="section-sub-banner bg-9">
+            <div class="awe-overlay"></div>
+            <div class="sub-banner">
+                <div class="container">
+                    <div class="text text-center">
+                        <h2>Payment Sucees</h2>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+        <!-- END / SUB BANNER -->
+
+        <!-- ABOUT -->
+        <section class="section-about">
+            <div class="container">
+                <div class="about" style="margin-top: 4px">
+                  Your payment was successfull. The 
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+@endsection
